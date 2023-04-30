222346e4826855a42a6eb9c8808f4baef497bf50
p2X0 <mwangedennis05@gmail.com>
2023-04-30 19:32:14 +0300

    added book photos

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG',default=1))
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1','http://127.0.0.1:5000/']
 
 
 # Application definition
